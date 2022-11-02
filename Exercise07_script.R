7c395f856fb14a4987799ed3d7d2a5aaf99c8577
Camden Arnold <carnold3@nd.edu>
2022-11-01 23:00:17 -0400

    Exercise 7 Submission

--- Exercise07_script.R
+++ Exercise07_script.R
@@ -1,9 +1,11 @@
-#Q1 
+#Assumes working directory is set to Exercise07 folder
 
-#Converts iris.csv into tab deliminated .txt file
-write.table(read.csv("iris.csv"), file="iris.txt", sep="\t")
+#Q1
 
-#Q2 
+#Converts iris.csv into tab-delimited .txt file
+write.table(read.csv("iris.csv"), file="iris.txt", sep="\t", row.names = FALSE, col.names = TRUE)
+
+#Q2
 
 #Creates a list to add elements to
 finalList=list()
@@ -12,16 +14,15 @@ finalList=list()
 finalList$element1=seq(from=100,to=1000,by=100)
 
 #Adds a two-row, two-column df with team names and score of Notre Dame football game to list
-team=c("ND","UNLV")
-score=c(44,21)
-finalList$element2=data.frame(team,score,stringsAsFactors=FALSE)
+TEAM=c("ND","UNLV")
+SCORE=c(44,21)
+finalList$element2=data.frame(TEAM,SCORE,stringsAsFactors=FALSE)
 
 #Adds the number 999 to list
 finalList$element3=999
 
 #Adds a 10-row, 5-column matrix with integer from 1-50 to list
-numbers=c(1:50)
-finalList$element4=matrix(numbers,nrow=10,ncol=5)
+finalList$element4=matrix(c(1:50),nrow=10,ncol=5)
 
 #Adds a vector containing three letters to list
 finalList$element5=c("a","b","c")
